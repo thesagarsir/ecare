4d9f69bf4d454ec5f1f61047da5c7bef341899fc
thesagarsir <74952980+thesagarsir@users.noreply.github.com>
2021-03-31 16:44:06 +0530

    Add files via upload

---
+++ src/main/java/com/ecare/poc/ecare/EcareApplication.java
@@ -0,0 +1,13 @@
+package com.ecare.poc.ecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecare/poc/ecare/EcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecare.poc.ecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
